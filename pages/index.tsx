afab31007151450d06fd8e0305d7039a48d8af89
Quentin <quentin.altieri@gmail.com>
2022-11-21 17:56:29 +0100

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { ResponsiveAreaBump } from '@nivo/bump'
-import logo from "*.gif";
 
 const data = [
   {
